0958316f087d582c22e3f028a505419176fe3a62
Larissa Cardoso <106749260+larissacard@users.noreply.github.com>
2022-06-07 10:08:45 -0300

    Create Exe01.go

---
+++ Exe01.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+
+func main() {
+   x := 13
+   y := "Arthut Aguiar"
+   z := true
+   
+   fmt.Println(x, y, z)
+   fmt.Println(x)
+   fmt.Println(y)
+   fmt.Println(z)
+    
+}
