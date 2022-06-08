82a9b5a80d7f037b146e383f1dac41b62960c935
Larissa Cardoso <106749260+larissacard@users.noreply.github.com>
2022-06-07 21:04:59 -0300

    Update Exe01.go

--- Exe01.go
+++ Exe01.go
@@ -2,13 +2,5 @@ package main
 import "fmt"
 
 func main() {
-   x := 13
-   y := "Arthut Aguiar"
-   z := true
-   
-   fmt.Println(x, y, z)
-   fmt.Println(x)
-   fmt.Println(y)
-   fmt.Println(z)
-    
+    fmt.Println("Hello World")
 }
